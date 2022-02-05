a653ba87c8e6d205fcfccc8b85c08da75b80e7a9
Nate Harris <nwithan8@users.noreply.github.com>
2022-02-05 16:14:32 -0700

    Update setup_python_pip.sh

--- scripts/setup_python_pip.sh
+++ scripts/setup_python_pip.sh
@@ -2,8 +2,4 @@
 sudo apt-get install python3-pip
 
 # Install pip packages
-declare -a packages=("important" "bestbuy" "pyualtrics" "twine" "Robinhood" "robin-stocks" "python-dotenv" "pyspark" "pyperclip" "pycrypto" "notifiers" "nltk" "lxml" "louis" "gpg" "GitPython" "geopy" "findspark" "future" "dropbox" "discord.py" "cryptography" "boto3" "colorama" "praw" "prawcore" "pysqlcipher3" "segno" "textblob" "tweepy" "GGA" "docutils" "python-dateutil" "six" "setuptools" "beautifulsoup4" "opencv-python" "Pillow" "discord" "plexapi" "requests" "asyncio" "zxcvbn" "numpy" "scipy" "pandas")
-for val in ${packages[@]}; do
-  echo Installing $val
-  sudo pip3 install $val
-done
+sudo pip3 install -r requirements.txt
