d1e026cb491f48f4bc158d0ea6f06c4b0bd155eb
nwithan8 <nwithan8@users.noreply.github.com>
2023-03-27 12:07:46 -0600

    - Note .NET installation oddity

--- setup/mac/langs/net.sh
+++ setup/mac/langs/net.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# This script SHOULD install both the SDK and runtime for each version of .NET
+# If you can't run a .NET app, you may need to install the runtime
+# If you can't build a .NET app, you may need to install the SDK
+# https://dotnet.microsoft.com/en-us/download/dotnet
+
 DOTNET_VERSIONS="Current 7.0 6.0 5.0 3.1"
 echo "Installing C# and .NET..."
 wget https://dot.net/v1/dotnet-install.sh
