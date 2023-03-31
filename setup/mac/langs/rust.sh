a107f9545ec767debfbb2958e6326a2b7bfa0452
nwithan8 <nwithan8@users.noreply.github.com>
2023-03-31 16:52:04 -0600

    - Install Rust - New shortcuts in bashrc

--- setup/mac/langs/rust.sh
+++ setup/mac/langs/rust.sh
@@ -2,4 +2,8 @@
 
 # This script will install Cargo, the Rust package manager
 
+echo "Installing Cargo..."
 curl https://sh.rustup.rs -sSf | sh
+
+# Add Cargo to PATH
+echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> ~/.bashrc
