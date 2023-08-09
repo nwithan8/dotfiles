639d84aec2d9c3be1d8cc143a54c0927777a24d7
nwithan8 <nwithan8@users.noreply.github.com>
2023-08-09 16:19:50 -0600

    - Organize .bashrc files, improve symlinking - Use gitignore to avoid committing internal-only bashrc files

--- setup/mac/files.sh
+++ setup/mac/files.sh
@@ -6,8 +6,18 @@ REPO_LOCATION=$1
 # Using symlinks instead of copying files allows for updates to this repo to be reflected on the user's system immediately
 
 # BashFTW profiles
-ln -s "$REPO_LOCATION/dot/.bashrc.nharris" ~/.bashrc.nharris || true
-ln -s "$REPO_LOCATION/dot/.bashrc.nharris.mac" ~/.bashrc.nharris.mac || true
+DOT_FOLDER="$REPO_LOCATION/dot"
+# for each directory in DOT_FOLDER, find all files starting with .bashrc and symlink them to the same name in ~
+for dir in "$DOT_FOLDER"/*; do
+    if [ -d "$dir" ]; then
+        for file in "$dir"/.bashrc*; do
+            FILE_NAME=$(basename "$file")
+            DEST_FILE="$HOME/$FILE_NAME"
+            echo "Symlinking $file to $DEST_FILE"
+            ln -s "$file" "$DEST_FILE" || true
+        done
+    fi
+done
 
 # Scripts
 SCRIPTS_DIR="$HOME/.scripts"
