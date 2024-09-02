8881fa9aed8fec0d7b986631e694ba9467f24e1c
Nate Harris <nwithan8@users.noreply.github.com>
2024-09-02 14:03:45 -0600

    - Use paths for YT Download script

--- scripts/youtube/download/download.sh
+++ scripts/youtube/download/download.sh
@@ -3,6 +3,9 @@
 # Read each line in the config file
 # Each line has the following format:
 # <FOLDER NAME>;<CHANNEL_OR_PLAYLIST_URL>;<OPTIONAL_FILTERS>;<FILE_NAME_TEMPLATE>
+CONFIG_FILE=$1
+ARCHIVE_FILE=$2
+
 while IFS=';' read -r folder url filters template; do
   # If line begins with #, skip it
   # shellcheck disable=SC3057
@@ -16,10 +19,10 @@ while IFS=';' read -r folder url filters template; do
   if [ -n "$filters" ]; then
     PRE_FLAGS="$PRE_FLAGS --match-filter \"$filters\""
   fi
-  POST_FLAGS="--write-auto-subs --write-subs --sub-lang en --convert-subs srt --restrict-filenames --download-archive \"archive.txt\""
+  POST_FLAGS="--write-auto-subs --write-subs --sub-lang en --convert-subs srt --restrict-filenames --download-archive \"$ARCHIVE_FILE\""
   # Download the videos
   # shellcheck disable=SC2086
   COMMAND="yt-dlp $PRE_FLAGS \"$url\" $POST_FLAGS"
   echo $COMMAND
   eval $COMMAND
-done < download_list.txt
+done < "$CONFIG_FILE"
