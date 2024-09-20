445ee9b8745b2e0ab7e94c1049eea04997c180fb
Nate Harris <nwithan8@users.noreply.github.com>
2024-09-20 09:43:33 -0600

    - Add GitHub Copilot aliases

--- setup/mac/github.sh
+++ setup/mac/github.sh
@@ -6,3 +6,6 @@ gh auth login --git-protocol ssh --web -s admin:public_key
 
 # Add SSH key to GitHub
 gh ssh-key add ~/.ssh/id_rsa.pub -t "$(hostname)"
+
+# Add GitHub Copilot extension
+gh extension install github/gh-copilot --force
