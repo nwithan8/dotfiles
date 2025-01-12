c92aee7201e013bf4433c29e20bc686a838cd03c
Nate Harris <nwithan8@users.noreply.github.com>
2025-01-11 18:40:04 -0700

    Fix Linux usage

---
+++ setup/linux/extras.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Extra, optional enhancements
+
+# Use bat rather than cat
+echo "Downloading improved cat command"
+apt-get install bat
+echo 'alias cat="bat"' | sudo tee -a ~/.bashrc
+
+# Set up key bindings to use fzf for history search
+echo "Setting up key bindings for fzf"
+"$(brew --prefix)"/opt/fzf/install
+source "$HOME/.bashrc"
