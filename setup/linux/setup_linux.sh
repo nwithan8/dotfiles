992c8a2d4c17ef04cc3725d411b46f3c216b0594
Nate Harris <nwithan8@users.noreply.github.com>
2025-01-11 22:43:30 -0700

    Add Linux setup script

---
+++ setup/linux/setup_linux.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Install git, gh and make
+sudo apt-get install git gh make
+
+# Set up GitHub authentication
+echo "Starting GitHub authentication. Select 'Skip' in the next prompt and follow the instructions."
+gh auth login --git-protocol ssh --web -s admin:public_key
+
+# Add SSH key to GitHub
+gh ssh-key add ~/.ssh/id_rsa.pub -t "$(hostname)"
+
+# Add GitHub Copilot extension
+gh extension install github/gh-copilot --force
+
+# Clone the dotfiles repo
+git clone git@github.com:nwithan8/dotfiles.git
+cd dotfiles/setup/linux
+
+# Run make step
+make fresh-install
