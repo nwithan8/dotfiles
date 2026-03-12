3e520ceff6fe3f41c5f2779a7eb899b98da34878
Nate Harris <nwithan8@users.noreply.github.com>
2026-03-12 13:45:52 -0600

    - Fix brew packages

--- setup/langs/php.sh
+++ setup/langs/php.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 PHP_VERSIONS="7.4 8.0 8.1 8.2" # Install latest of all listed PHP versions
+brew tap shivammathur/php
 for V in $PHP_VERSIONS; do
   echo "Installing PHP $V..."
   brew install shivammathur/php/php@"$V"
